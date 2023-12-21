ff269797a46814f9eb99ffaaa69b38298976779c
Ali Nik <alinikandish001@gmail.com>
2023-12-21 10:59:12 +0330

    Fix : change categories items to dynamic

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,27 +1,41 @@
+import { Category } from "@/types";
 import Link from "next/link";
 import React from "react";
 
-const Categories = () => {
+
+const getCategories = async (): Promise<Category[] | null> => {
+  try {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/categories`);
+
+    if (res.ok) {
+      const categories = await res.json();
+      return categories;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
+};
+
+
+const Categories = async () => {
+  const categories = await getCategories();
+  console.log(categories);
+  
+
   return (
     <div className="mt-5">
       <h3>موضوعات:</h3>
 
       <div className="flex gap-x-2 mt-2">
-        <Link href='' className="bg-blue-500 text-blue-100 py-2 px-4 rounded-full text-xs font-bold flex items-center">
-          سرگرمی
+
+        {categories?.map(category =>{
+          return(<Link href={`/categories/${category.catName}`} 
+          className="bg-blue-500 text-blue-100 py-2 px-4 rounded-full text-xs font-bold flex items-center">
+              {category.catName}
         </Link>
-        <Link href='' className="bg-blue-500 text-blue-100 py-2 px-4 rounded-full text-xs font-bold flex items-center">
-          ورزشی
-        </Link>
-        <Link href='' className="bg-blue-500 text-blue-100 py-2 px-4 rounded-full text-xs font-bold flex items-center">
-          برنامه نویسی
-        </Link>
-        <Link href='' className="bg-blue-500 text-blue-100 py-2 px-4 rounded-full text-xs font-bold flex items-center">
-          شبکه
-        </Link>
-        <Link href='' className="bg-blue-500 text-blue-100 py-2 px-4 rounded-full text-xs font-bold flex items-center">
-          دیگر
-        </Link>
+        )
+        })}
       </div>
     </div>
   );
