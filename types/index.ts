5748572acedbcad11538e2e3088d2e1ec7cdd946
Ali Nik <alinikandish001@gmail.com>
2023-12-21 09:00:19 +0330

    Feat:Add Types

---
+++ types/index.ts
@@ -0,0 +1,19 @@
+export type Category = {
+  id: string;
+  catName: string;
+};
+
+export type Post = {
+  id: string;
+  title: string;
+  content: string;
+  imageUrl?: string;
+  publicId?: string;
+  catName?: string;
+  links: null | string[];
+  createdAt: string;
+  authorEmail: string;
+  author: {
+    name: string;
+  };
+};
