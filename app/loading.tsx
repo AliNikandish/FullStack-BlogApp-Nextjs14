4a283465af68c7c770eb30435e99bee614e297a0
Ali Nik <alinikandish001@gmail.com>
2023-12-21 14:58:59 +0330

    Feat:Add Loading Page

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="flex gap-4 flex-wrap justify-center items-center min-h-[500px]">
+    <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-black" />
+</div>
+  )
+}
+
+export default loading
